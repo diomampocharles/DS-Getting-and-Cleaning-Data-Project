65a30d089ad42c23cd0dcbb8f5c5b26ca209fb5d
Charles Rey E. Diomampo <diomampocharles@gmail.com>
2019-04-12 09:28:45 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,47 +1,43 @@
-filename <- "Coursera_DS3_Final.zip"
-
-# Checking if archieve already exists.
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileURL, filename, method="curl")
+  download.file(fileURL, "Course 3", method="curl")
 }  
 
-# Checking if folder exists
 if (!file.exists("UCI HAR Dataset")) { 
-  unzip(filename) 
+  unzip("Course 3") 
 }
 
-features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+features <- read.table("./features.txt", col.names = c("n","functions"))
+activities <- read.table("./activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("./test/subject_test.txt", col.names = "subject")
+x_test <- read.table("./test/X_test.txt", col.names = features$functions)
+y_test <- read.table("./test/y_test.txt", col.names = "code")
+subject_train <- read.table("./train/subject_train.txt", col.names = "subject")
+x_train <- read.table("./train/X_train.txt", col.names = features$functions)
+y_train <- read.table("./train/y_train.txt", col.names = "code")
 
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
-Merged_Data <- cbind(Subject, Y, X)
+Merged_Data1 <- cbind(Subject, Y, X)
 
-TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+Merged_Data2 <- Merged_Data1 %>% select(subject, code, contains("mean"), contains("std"))
 
-TidyData$code <- activities[TidyData$code, 2]
+Merged_Data2$code <- activities[Merged_Data2$code, 2]
 
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+names(Merged_Data2)[2] = "activity"
+names(Merged_Data2)<-gsub("Acc", "Accelerometer", names(Merged_Data2))
+names(Merged_Data2)<-gsub("Gyro", "Gyroscope", names(Merged_Data2))
+names(Merged_Data2)<-gsub("BodyBody", "Body", names(Merged_Data2))
+names(Merged_Data2)<-gsub("Mag", "Magnitude", names(Merged_Data2))
+names(Merged_Data2)<-gsub("^t", "Time", names(Merged_Data2))
+names(Merged_Data2)<-gsub("^f", "Frequency", names(Merged_Data2))
+names(Merged_Data2)<-gsub("tBody", "TimeBody", names(Merged_Data2))
+names(Merged_Data2)<-gsub("-mean()", "Mean", names(Merged_Data2), ignore.case = TRUE)
+names(Merged_Data2)<-gsub("-std()", "STD", names(Merged_Data2), ignore.case = TRUE)
+names(Merged_Data2)<-gsub("-freq()", "Frequency", names(Merged_Data2), ignore.case = TRUE)
+names(Merged_Data2)<-gsub("angle", "Angle", names(Merged_Data2))
+names(Merged_Data2)<-gsub("gravity", "Gravity", names(Merged_Data2))
 
-FinalData <- TidyData %>% group_by(subject, activity) %>% summarise_all(funs(mean))
+FinalData <- Merged_Data2 %>% group_by(subject, activity) %>% summarise_all(funs(mean))
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
